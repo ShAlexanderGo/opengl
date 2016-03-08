8ea03dd3921756deffb684917d99385cf9ab7cc9
Shtanko <Shtanko-mephi@yandex.ru>
2016-03-08 19:13:59 +0300

    rotation

--- star.c
+++ star.c
@@ -3,21 +3,42 @@
 #include <math.h>
 #include <GL/glut.h>
 
-void pyramid() {
+float gangle = 0;
+
+void pyramid(float angle) {
+  glRotatef(angle, 0, 0, 1);
+  
   glBegin(GL_TRIANGLE_FAN);
     glVertex3f(0, 0, 0);
-    glVertex3f(10, 10, 20);
-    glVertex3f(-10, 10, 20);
-    glVertex3f(-10, -10, 20);
-    glVertex3f(10, -10, 20);
-    glVertex3f(10, 10, 20);
+    glVertex3f(10, 20, 10);
+    glVertex3f(-10, 20, 10);
+    glVertex3f(-10, 20, -10);
+    glVertex3f(10, 20, -10);
+    glVertex3f(10, 20, 10);
+  glEnd();
+  glBegin(GL_QUADS);
+    glVertex3f(10, 20, 10);
+    glVertex3f(-10, 20, 10);
+    glVertex3f(-10, 20, -10);
+    glVertex3f(10, 20, -10);
   glEnd();
 }
 
 void display(void) {
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-  pyramid();
+  glLoadIdentity();
+  pyramid(gangle);
   glutSwapBuffers();
+}
+
+void keyboard(unsigned char key, int x, int y){
+}
+
+void keyboard2(int key, int x, int y){
+  if( key == GLUT_KEY_UP ){
+    gangle += 10;
+    display();  
+  }
 }
 
 void init(void) {
@@ -35,6 +56,7 @@ void init(void) {
   glOrtho(-50, 50, -50, 50, -50, 50);
 
   //gluPerspective(50.0, 1.0, 1.0, 15.0);
+  glMatrixMode(GL_MODELVIEW);
 }
 
 int main(int argc, char **argv) {
@@ -42,6 +64,8 @@ int main(int argc, char **argv) {
   glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
   glutCreateWindow("Star");
   glutDisplayFunc(display);  
+  glutKeyboardFunc(keyboard);
+  glutSpecialFunc(keyboard2);
   init();
   glutMainLoop();
   return 0;             
