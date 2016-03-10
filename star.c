487592e41948966e71a46624848e2157cd5aa127
Shtanko <Shtanko-mephi@yandex.ru>
2016-03-10 08:13:20 +0300

    star finished

--- star.c
+++ star.c
@@ -3,28 +3,43 @@
 #include <math.h>
 #include <GL/glut.h>
 
+const float ANGLE_X_STEP = 2;
+const float ANGLE_Y_STEP = 2;
+const float SCALE_STEP = 1.1; //>1.0
+const float SPEED = 1.5;
+
+const float ANGLE_WIDTH = 10.0; //how flat pyramids
+const float LENGTH = 30.0; //how long pyramids
+const float SPHERE_RADIUS = 5.0;
+
+const float SPHERE_SHIFT_MIN = 35.0;
+const float SPHERE_SHIFT_MAX = 45.0;
+
+const float PYRAMID_COLOR[] = {1.0, 0.0, 1.0};
+const float SPHERE_COLOR[] = {0.0,0.0,1.0};
+
+const float LIGHT_DIF[] = {1.0, 1.0, 1.0, 1.0};
+const float LIGHT_POS[] = {-1.0, -1.0, 1.0, 0.0};
+
 float angleX = 0; //any
 float angleY = 0; //-90 +90
-float shift = 0;
-float speed = 2;
+float shift = 35;
+float anDir = 1.0;
 float scale = 1.0;
+int animated = 0;
 
-float gangle = 0;
-float gx = 0;
-float gy = 0;
-float gangleUp = 0;
-float gangleRot = 0;
-
+//Degrees to radians
 float toRadians(float angle) {
   return angle * (M_PI / 180);
 }
 
+//triangle along y axis, with angleRot rotation, angleUp - angle from axis y
 void triangle(float angleUp, float angleRot, float length) {
   glPushMatrix();
   glRotatef(angleRot, 0, 1, 0);
   glRotatef(angleUp, 1, 0, 0);
 
-  glColor3f(1.0,0.0,1.0);
+  glColor3fv(PYRAMID_COLOR);
   glBegin(GL_TRIANGLES);
     glNormal3d(0, 0, 1);
     glVertex3f(0, 0, 0);
@@ -36,10 +51,9 @@ void triangle(float angleUp, float angleRot, float length) {
   glPopMatrix();
 }
 
-void pyramid(float angle, float angleWidth, float length) {
-  //glTranslatef(gx, gy, 0);
+//angleWidth - how flat pyramid
+void pyramid(float angleWidth, float length) {
   glPushMatrix();
-  //glRotatef(angle, 0, 0, 1);
   triangle(angleWidth, 0, length);
   triangle(angleWidth, 90, length);
   triangle(angleWidth, 180, length);
@@ -64,13 +78,14 @@ void pyramid(float angle, float angleWidth, float length) {
   glPopMatrix();
 }
 
+//angle - direction of petal
 void petal(float angle) {
   glPushMatrix();
   glRotatef(angle, 0, 0, 1);
-  pyramid(0, 10, 20);
-  glTranslatef(0, 35+shift, 0);
-  glColor3f(0.0,0.0,1.0);
-  glutSolidSphere(5, 100, 100);
+  pyramid(ANGLE_WIDTH, LENGTH);
+  glTranslatef(0, shift, 0);
+  glColor3fv(SPHERE_COLOR);
+  glutSolidSphere(SPHERE_RADIUS, 100, 100);
   glPopMatrix();
 }
 
@@ -82,13 +97,8 @@ void display(void) {
   glRotatef(angleY, 1, 0, 0);
   glRotatef(angleX, 0, 1, 0);
 
-  
-  GLfloat light_diffuse[] = {1.0, 1.0, 1.0, 1.0};
-  GLfloat light_position[] = {-1.0, -1.0, 1.0, 0.0};
-
-  glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
-  glLightfv(GL_LIGHT0, GL_POSITION, light_position);
-  //glRotatef(100, -10, 0, 0);
+  glLightfv(GL_LIGHT0, GL_DIFFUSE, LIGHT_DIF);
+  glLightfv(GL_LIGHT0, GL_POSITION, LIGHT_POS);
   petal(0);
   petal(90);
   petal(180);
@@ -96,36 +106,34 @@ void display(void) {
   glutSwapBuffers();
 }
 
-int animated = 0;
-
 void keyboard(unsigned char key, int x, int y){
  if (key == 'p') {
    animated = 0;
  } else if (key == 's') {
    animated = 1;
  } else if (key == '+') {
-   scale *= 1.1;
+   scale *= SCALE_STEP;
  } else if (key == '-') {
-   scale /= 1.1;
+   scale /= SCALE_STEP;
  }
 }
 
 void keyboard2(int key, int x, int y){
   if( key == GLUT_KEY_UP ){
-    angleY += 2;
+    angleY += ANGLE_Y_STEP;
     if (angleY > 90)
       angleY = 90;
     display();  
   } else if( key == GLUT_KEY_DOWN ){
-    angleY -= 2;
+    angleY -= ANGLE_Y_STEP;
     if (angleY < -90)
       angleY = -90;
     display();  
   } else if( key == GLUT_KEY_RIGHT ){
-    angleX += 2;
+    angleX += ANGLE_X_STEP;
     display();  
   } else if( key == GLUT_KEY_LEFT ){
-    angleX -= 2;
+    angleX -= ANGLE_X_STEP;
     display();  
   }
 }
@@ -140,17 +148,19 @@ void init(void) {
 
   glMatrixMode(GL_PROJECTION);
   //glOrtho(-50, 50, -50, 50, -50, 50);
-
   gluPerspective(50.0, 1.0, 1.0, 1000.0);
   glMatrixMode(GL_MODELVIEW);
 }
 
 void update(int value) {
   if (animated) {
-    shift += speed;
+    shift += anDir * SPEED;
   }
-  if ((shift > 10) || (shift < 0)) {
-    speed *= -1;
+  if (shift > SPHERE_SHIFT_MAX) {
+    anDir = -1.0;
+  }
+  if (shift < SPHERE_SHIFT_MIN) {
+    anDir = 1.0;
   }
   glutTimerFunc(30, update, 0);
   display();
